50884fcd307b85ded496f1d91ac8860ecc0fc768
Soul <sornwilliam@gmail.com>
2020-07-30 17:51:56 -0500

    made api calls. Website functions okay not storing in mongo

--- routes/api_controller.js
+++ routes/api_controller.js
@@ -1,5 +1,44 @@
-const Exercises = require("../model/dbExercise");
+const Exercises = require("../models/dbExercise.js");
 
 module.exports = function(app) {
     //insert api calls 
+    app.get("/api/workouts/", (req,res) => {
+        Exercises.find({}).then(exerciseDB => {
+            res.json(exerciseDB);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
+
+    app.post("/api/workouts", ({body}, res) => {
+        const exercise = new Exercises(body);
+        Exercises.create(exercise).then(exerciseDB => {
+            res.json(exerciseDB);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
+
+    app.get("/api/workouts", (req, res) => {
+        Exercises.find({}).then(exerciseDB => {
+            res.json(exerciseDB);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
+
+    app.put("/api/workouts/:id", (req,res) => {
+        Exercises.findOneAndUpdate(
+            {_id: req.param.id},
+            {$push: {exercises: req.body}}
+        ).then(exerciseDB => {
+            res.json(exerciseDB);
+        })
+        .catch(err =>{
+            res.json(err);
+        });
+    });
 };
